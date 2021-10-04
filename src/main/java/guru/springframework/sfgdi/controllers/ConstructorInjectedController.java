9c6f3db715e8398e67ab14751932b9f4f52b214d
acrajag <andre.raja@gmail.com>
2021-10-04 16:57:15 +0100

    :sparkles: Using Qualifier() annotation

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,7 +10,7 @@ public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
 //    @Autowired        // -> Optional in constructor
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 

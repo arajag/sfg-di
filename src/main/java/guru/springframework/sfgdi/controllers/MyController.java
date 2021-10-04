279c0d6a41e23d0f4b5903d4ecc6af183cff9e68
acrajag <andre.raja@gmail.com>
2021-10-04 14:13:26 +0100

    :sparkles: Injections without Spring

---
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.controllers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello World!!!");
+        return "Hi Folks!";
+    }
+}
